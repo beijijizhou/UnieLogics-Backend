2411504b61be55f14b90691996953cb4beddc1a0
Sorin-Alin Pavel <sorinalinpavel@gmail.com>
2022-01-11 16:56:54 +0200

    added email template for reset password

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -47,19 +47,63 @@ const forgotPassword = async (req, res) => {
       to: email,
       subject: "Reset your password",
       text: "Reset your password",
-      html: `<div>
-        <h2>Reset your password.</h2>
-          <p>We're sorry you have forgotten your password.  Please follow the instructions below to reset your password.
-        </p>
-        <p>
-          Log in <a href="https://www.asinmice.com/login" style="text-decoration:underline;color:red;">here</a> using your username and the temporary password <span style="color:red;"><b>${randomPassword}</b></span>
-        </p>
-        <p>
-          Once logged in, please go to your profile settings and change the temporary password to a permanent one.
-        </p>
-        <p>If you have any question about your account, please email us at <a style="text-decoration: underline;" href="mailto:${from_who}?Subject=Reset%20password%20issue">${from_who}</a></p><br />
-        <p>Thank you again. Happy sourcing!<br /><br />
-      </div>`,
+      html: `<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+          <td>
+              <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                align="center" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                      <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                          style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                          <tr>
+                            <td style="height:40px;">&nbsp;</td>
+                          </tr>
+                          <tr>
+                            <td><img src="https://www.asinmice.com/public/logo.svg" alt="asinmice-logo" style="width:120px;margin-bottom:15px;"/></td>
+                          </tr>
+                          <tr>
+                            <td style="padding:0 35px;">
+                                <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+                                  requested to reset your password
+                                </h1>
+                                <span
+                                  style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                  We cannot simply send you your old password, but in order to help you out, a unique password has been generated for you.<br /> The new password is: <span style="color:red;"><b>${randomPassword}</b></span><br />
+                                  To login, please click the below button and use the newly generated password.
+                                </p>
+                                <a href="https://www.asinmice.com/login"
+                                  style="background:#a78c36;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Go to Login</a>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td style="height:40px;">&nbsp;</td>
+                          </tr>
+                      </table>
+                    </td>
+                <tr>
+                    <td style="height:20px;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;">
+                      <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong ><a href="https://www.asinmice.com" style="color:#a78c36;">www.asinmice.com</a></strong></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="height:80px;">&nbsp;</td>
+                </tr>
+              </table>
+          </td>
+        </tr>
+    </table>`,
     };
 
     mailgun.messages().send(mailBody, (sendError, body) => {
