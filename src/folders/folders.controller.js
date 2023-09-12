9579e8d4bc19237d893d43a7e47621cf5a02f685
sorinalinpavel <sorinalinpavel@gmail.com>
2023-09-12 23:56:16 +0300

    changed response to be folders directly

--- src/folders/folders.controller.js
+++ src/folders/folders.controller.js
@@ -29,7 +29,7 @@ const getAllFoldersForSpecificUser = async (req, res) => {
         status: "success",
         response: {
           email,
-          folders: foldersAfterDefault,
+          ...foldersAfterDefault,
         },
       });
     }
