30294c4fd1a0785ec8ed9e7e03c741f594a2b6e4
alwaz <alwazk8@gmail.com>
2024-11-25 22:58:04 +0500

    dbstring

--- src/connect/mongodb.js
+++ src/connect/mongodb.js
@@ -1,32 +1,37 @@
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
 
+// Use your working connection string here
+const dbURI = "mongodb+srv://userCompass:user12345@cluster0.tkxsn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongoose connected to MongoDB successfully!");
+  } catch (err) {
+    console.error("Mongoose connection error:", err.message);
+  }
+}
 
-const db = "mongodb+srv://userCompass:user12345@cluster0.tkxsn.mongodb.net/myFirstDatabase";
-// const db = process.env.MONGODB;
-mongoose.connect(db, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+// Monitor connection events
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose default connection is open.");
 });
 
-mongoose.connection.on("connected", function () {
-  console.log("Mongoose default connection open to " + process.env.MONGODB);
+mongoose.connection.on("error", (err) => {
+  console.error(`Mongoose default connection error: ${err}`);
 });
 
-mongoose.connection.on("error", function (err) {
-  console.log("Mongoose default connection error: " + err);
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose default connection is disconnected.");
 });
 
-mongoose.connection.on("disconnected", function () {
-  console.log("Mongoose default connection disconnected");
+// Handle app termination gracefully
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  console.log("Mongoose connection is disconnected due to app termination.");
+  process.exit(0);
 });
 
-process.on("SIGINT", function () {
-  mongoose.connection.close(function () {
-    console.log("Mongoose default connection disconnected through app termination");
-    process.exit(0);
-  });
-});
-// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
-// by default, you need to set it to false.
-mongoose.set("useFindAndModify", false);
+connectToDatabase();
